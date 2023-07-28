3f1b31406048e283b5553b003dcd1f15eda90552
aliaksandr.tsviatkou <aletsv@gmail.com>
2023-07-28 16:07:30 +0300

    Fix security

--- src/main/java/com/andreis/pet/project/btpapp/config/SecurityConfiguration.java
+++ src/main/java/com/andreis/pet/project/btpapp/config/SecurityConfiguration.java
@@ -3,51 +3,41 @@ package com.andreis.pet.project.btpapp.config;
 import com.sap.cloud.security.xsuaa.XsuaaServiceConfiguration;
 import com.sap.cloud.security.xsuaa.token.TokenAuthenticationConverter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
-public class SecurityConfiguration {
-
-    private final XsuaaServiceConfiguration xsuaaServiceConfiguration;
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    public SecurityConfiguration(XsuaaServiceConfiguration xsuaaServiceConfiguration) {
-        this.xsuaaServiceConfiguration = xsuaaServiceConfiguration;
-    }
+    XsuaaServiceConfiguration xsuaaServiceConfiguration;
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
         http
                 .sessionManagement()
-                // session is created by approuter
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // session is created by approuter
                 .and()
-                // demand specific scopes depending on intended request
                 .authorizeRequests()
-
                 .antMatchers("/**").authenticated()
-                .anyRequest().denyAll() // deny anything not configured above
+//                .antMatchers("/**").hasAuthority("Display")
+                .anyRequest().denyAll()
                 .and()
-                .oauth2ResourceServer().jwt()
+                .oauth2ResourceServer()
+                .jwt()
                 .jwtAuthenticationConverter(getJwtAuthoritiesConverter());
-
-        return http.build();
     }
 
     Converter<Jwt, AbstractAuthenticationToken> getJwtAuthoritiesConverter() {
         TokenAuthenticationConverter converter = new TokenAuthenticationConverter(xsuaaServiceConfiguration);
-        converter.setLocalScopeAsAuthorities(true);
+        converter.setLocalScopeAsAuthorities(true); // not applicable in case of multiple Xsuaa bindings!
         return converter;
     }
 }
